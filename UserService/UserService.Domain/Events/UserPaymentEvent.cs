4438fc3a0874ecf6e53578a7248618f7e824dc87
Paulo Anjos <pcsar.anjos@gmail.com>
2025-10-03 19:06:55 -0300

    feat: add Domain layer and UserPaymentEvent

---
+++ UserService/UserService.Domain/Events/UserPaymentEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Domain.Events
+{
+    public class UserPaymentEvent
+    {
+        public Guid TransactionId { get; init; }
+        public Guid AccountFrom { get; init; }
+        public Guid AccountTo { get; init; }
+        public string? PaymentMethod { get; init; } 
+
+        public int PaymentAmount { get; init; }
+        public DateTime Created { get; init; }
+
+        public UserPaymentEvent(Guid transactionId, Guid accountFrom, Guid accountTo, string? paymentMethod, int paymentAmount)
+        {
+            TransactionId = transactionId;
+            AccountFrom = accountFrom;
+            AccountTo = accountTo;
+            PaymentMethod = paymentMethod;
+            PaymentAmount = paymentAmount;
+            Created = DateTime.Now;
+        }
+    }
+}
