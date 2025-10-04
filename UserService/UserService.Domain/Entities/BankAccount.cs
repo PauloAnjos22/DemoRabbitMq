7d568600289e561e5ee45e561a3cec4325878408
Paulo Anjos <pcsar.anjos@gmail.com>
2025-10-03 22:23:10 -0300

    refactor(BankAccount entity): changing Ownsership attribute to CustomerId

--- UserService/UserService.Domain/Entities/BankAccount.cs
+++ UserService/UserService.Domain/Entities/BankAccount.cs
@@ -3,7 +3,7 @@
     public class BankAccount
     {
         public Guid Id { get; set; }
-        public Guid Ownership { get; set; }
+        public Guid CustomerId { get; set; }
         public int MoneyAmount { get; set; }
         public string? AccountType { get; set; }
 
