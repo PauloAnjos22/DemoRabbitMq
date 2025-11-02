03badd50976c5773d67484e63c56e00d4ce3380e
Paulo Anjos <pcsar.anjos@gmail.com>
2025-11-02 14:54:48 -0300

    refactor: add new services into PaymentCustomer UseCase

--- UserService/UserService.Application/UseCases/PaymentCustomer.cs
+++ UserService/UserService.Application/UseCases/PaymentCustomer.cs
@@ -1,53 +1,39 @@
-﻿using AutoMapper;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using UserService.Application.DTOs.Common;
 using UserService.Application.DTOs.Payment;
-using UserService.Application.Interfaces.Messaging;
 using UserService.Application.Interfaces.Repositories;
 using UserService.Application.Interfaces.Services;
 using UserService.Application.Interfaces.UseCases;
 using UserService.Domain.Entities;
-using UserService.Domain.Events;
 
 namespace UserService.Application.UseCases
 {
     public class PaymentCustomer : IEventPayment
     {
-        private readonly ICustomerRepository _customerRepository;
-        private readonly IMapper _mapper;
         private readonly IPaymentRepository _paymentRepository;
-        private readonly IBankAccountRepository _bankAccountRepository;
-        private readonly IMessagePublisher<CustomerPaymentEvent> _paymentPublisher;
-        private readonly IMessagePublisher<TransactionCompletedEvent> _transactionPublisher;
         private readonly IEfUnitOfWork _efUnitOfWork;
         private readonly ILogger<PaymentCustomer> _logger;
         private readonly IPaymentValidatorService _paymentValidator;
         private readonly IBankAccountService _bankAccountService;
+        private readonly IPaymentEventPublisherService _paymentEventPublisher;
 
 
         public PaymentCustomer(
-            ICustomerRepository customerRepository,
-            IMapper mapper,
             IPaymentRepository paymentRepository,
-            IMessagePublisher<CustomerPaymentEvent> paymentPublisher,
-            IMessagePublisher<TransactionCompletedEvent> transactionPublisher,
             IBankAccountRepository bankAccountRepository,
             IEfUnitOfWork efUnitOfWork,
             ILogger<PaymentCustomer> logger,
             IPaymentValidatorService paymentValidator,
-            IBankAccountService bankAccountService
+            IBankAccountService bankAccountService,
+            IPaymentEventPublisherService paymentEventPublisher
             )
         {
-            _customerRepository = customerRepository;
-            _mapper = mapper;
             _paymentRepository = paymentRepository;
-            _paymentPublisher = paymentPublisher;
-            _transactionPublisher = transactionPublisher;
-            _bankAccountRepository = bankAccountRepository;
             _efUnitOfWork = efUnitOfWork ?? throw new ArgumentNullException(nameof(efUnitOfWork));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _paymentValidator = paymentValidator;
             _bankAccountService = bankAccountService;
+            _paymentEventPublisher = paymentEventPublisher;
         }
 
         public async Task<ResultResponse> ProcessPayment(CreatePaymentRequest request)
@@ -55,8 +41,7 @@ namespace UserService.Application.UseCases
             var validationResult = await _paymentValidator.ValidateAsync(request );
             if (!validationResult.Success)
             {
-                var failTransactionEvent = new TransactionCompletedEvent(Guid.NewGuid(), request.From, request.To, request.Amount, "BRL", request.Method, "Fail", "Insufficient funds");
-                var failtransactionPublished = await _transactionPublisher.PublishAsync(failTransactionEvent);
+                await _paymentEventPublisher.PublishPaymentFailedAsync(request.From, request.To, request.Amount, request.Method, "Insufficient funds or account missing"); // Services/PaymentEventPublisher
                 return ResultResponse.Fail(validationResult.ErrorMessage ?? "Validation result failed");
             }
 
@@ -67,7 +52,7 @@ namespace UserService.Application.UseCases
                 var transferOk = await _bankAccountService.TransferAsync(request.From, request.To, request.Amount);
                 if (!transferOk)
                 {
-                    await _transactionPublisher.PublishAsync(new TransactionCompletedEvent(Guid.NewGuid(), request.From, request.To, request.Amount, "BRL", request.Method, "Fail", "Insufficient funds or account missing"));
+                    await _paymentEventPublisher.PublishPaymentFailedAsync(request.From, request.To, request.Amount, request.Method, "Insufficient funds or account missing"); // Services/PaymentEventPublisher
                     await _efUnitOfWork.RollbackTransactionAsync();
                     return ResultResponse.Fail("Transaction failed");
                 }
@@ -83,11 +68,7 @@ namespace UserService.Application.UseCases
                 await _paymentRepository.SaveAsync(payment);
                 await _efUnitOfWork.CommitTransactionAsync();
 
-                var paymentEvent = new CustomerPaymentEvent(payment.Id, payment.From, payment.To, payment.Method, payment.Amount);
-                var transactionEvent = new TransactionCompletedEvent(payment.Id, payment.From, payment.To, payment.Amount, "BRL", payment.Method, "Completed", "");
-
-                await _paymentPublisher.PublishAsync(paymentEvent);
-                await _transactionPublisher.PublishAsync(transactionEvent);
+                await _paymentEventPublisher.PublishPaymentCompletedAsync(payment); // Services/PaymentEventPublisher
 
                 return ResultResponse.Ok();
             }
